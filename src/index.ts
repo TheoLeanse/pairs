4071594faa822eaf88e754f6e419d239ea1a1ed0
theoleanse <theoleanse@gmail.com>
2018-11-08 11:14:38 +0000

    add pair config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import * as R from 'ramda';
 import shuffle from './lib/shuffle';
+import * as config from '../pairing-config.json';
 
 export const pair = R.compose(
 	R.splitEvery(2),
 	shuffle
 );
+
+export default () => pair(config.in);
+
+console.log(pair(config.in));
